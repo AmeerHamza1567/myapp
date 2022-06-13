f552bd6bac9faa1225c7f6c0d2e54688104ffbbf
AmeerHamza1567 <ameerhamza1567@gmail.com>
2022-06-13 14:37:40 +0500

    Updates

---
+++ precache-manifest.c841cb0231b5d4a81984df2731cf84d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51fa026c9ba195dff6a3",
+    "url": "/myapp/static/css/main.5ee477d7.chunk.css"
+  },
+  {
+    "revision": "51fa026c9ba195dff6a3",
+    "url": "/myapp/static/js/main.51fa026c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/myapp/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/myapp/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "4cb993373d4464ca5702",
+    "url": "/myapp/static/js/runtime~main.4cb99337.js"
+  },
+  {
+    "revision": "9533357a534ad242ec01c0866d4797fd",
+    "url": "/myapp/index.html"
+  }
+];
